7d7ffe58f2669f85509225f2c09042c4ef1f83ff
oliveira-gu <gu2018oliveirasl@gmail.com>
2025-01-08 21:39:35 -0300

    sending the second part of our guessing game

--- guessingGame.c
+++ guessingGame.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 int main(){
     
     // Print the guessing game head
@@ -8,11 +9,47 @@ int main(){
     printf("********************************\n");
 
     // Store the secret number in a variable
-    int secretNumber;
-    
-    printf("What is your guess? ");
-    scanf("%d", &secretNumber);
-    printf("Oh, so %d is your guess? ", secretNumber);
+    int secretNumber = 10;
+    int playerAttempts = 1;
+    int playerGuess;
+    int playerWin = 0;
 
+    // Building the game menu
+    while (playerWin == 0){
+
+        printf("Attempt %d\n", playerAttempts);
+        printf("What is your guess? ");
+        scanf("%d", &playerGuess);
+
+        if(playerGuess < 0){
+            printf("Your guess cannot be a negative number!\n");
+            continue;
+        }
+
+        // printf("Oh, so %d is your guess? ", playerGuess);
+
+        // Building the code's conditionals
+        int hit = (playerGuess == secretNumber);
+        int greater = playerGuess > secretNumber;
+
+        if(hit){
+            printf("Congrats! You've got it right.\n");
+            playerWin = 1;
+        }
+
+        else if (greater){
+            printf("Your guess was greater than the secret number.\n");
+        } 
+
+        else {
+            printf("Your guess was less than the secret number.\n");
+        }
+        
+        playerAttempts++;
+    }
+    
+    printf("\nEnd game\n");
+    printf("You finished the game after %d attempts\n", playerAttempts-1);
     return 0;
+
 }
